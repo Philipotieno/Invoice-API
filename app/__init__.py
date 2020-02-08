3346c800c011745a8903692e92886fe26f24db65
philipotieno <tmitchellb007@gmail.com>
2020-02-09 01:48:29 +0300

    feat: adds endpoint to view last 30 transactions

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import csv
 from operator import itemgetter
-
-from flask import Flask, abort, jsonify
+import datetime
+from flask import Flask, abort, jsonify, request
 
 from app.models import Database, Invoice
 from instance.config import app_config
@@ -23,46 +23,66 @@ def create_app(config_name):
         try:
             filename = 'SalesInvoiceTemplate.csv'
             new_filename = 'NewSalesInvoiceTemplate.csv'
-            
+
             with open(filename, 'r') as f:
-                
+
                 # creating a csv reader object
                 reader = csv.reader(f)
                 with open(new_filename, 'w') as output_file:
                     writer = csv.writer(output_file, delimiter=',')
-                    
+
                     # Get specific columns and write them on the new csv file
                     writer.writerows(
                         map(itemgetter(0, 10, 12, 13, 16, 17, 18), reader))
-                    
+
                 # Open new file and copy the items to the invoices table
                 with open(new_filename, 'r') as r:
                     reader = csv.reader(r)
                     cmd = "COPY invoices FROM STDIN WITH (FORMAT CSV, HEADER TRUE)"
                     cur.copy_expert(cmd, r)
                     db.conn.commit()
-                        
+
                 # Covert the csv file into a dictionary
                 dictreader = csv.reader(open(new_filename))
                 result = {}
                 for row in dictreader:
-                    result[row[0]]=row[1:]
-                            
+                    result[row[0]] = row[1:]
+
                 return jsonify({
                     'success': True,
                     'invoice': result
-                    }), 201
+                }), 201
         except Exception as e:
-            return jsonify({"Error" : str(e)}), 422
+            return jsonify({"Error": str(e)}), 422
 
-    @app.route('/invoices/', methods=['GET'])
+    @app.route('/invoices', methods=['GET'])
     def get_top_invoices():
         invoices = Invoice.get_top_customers()
         if invoices:
             return jsonify({
                 'Invoice': invoices,
-                'total' :len(invoices)
-                }), 200
+                'total': len(invoices)
+            }), 200
         return jsonify({'message': 'No invoices are available!'}), 404
-    
+
+    @app.route('/invoices/transactions', methods=['POST'])
+    def get_transactions():
+        data = request.get_json()['date']
+
+        if datetime.datetime.strptime(data, '%Y-%m-%d'):
+            query = "SELECT contactname, duedate,\
+                    (quantity*unitamount) as totalamountdue from invoices\
+                    WHERE duedate > '{}' ORDER BY duedate DESC LIMIT 30;".format(data)
+            cur.execute(query)
+            invoices = cur.fetchall()
+            if invoices:
+                return jsonify({
+                    'Invoice': invoices,
+                    'total': len(invoices)
+                }), 200
+            return jsonify({'message': 'No invoices are available for the provided date'}), 404
+        return jsonify({
+            "message": "wrong date format"
+        })
+
     return app
