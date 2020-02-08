2acc0094d223e97cda60c2a3f3a584110eecc53e
Philipotieno <tmitchellb007@gmail.com>
2020-02-08 15:30:29 +0300

    feat: modify model datatype

--- app/models.py
+++ app/models.py
@@ -7,8 +7,10 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 conn = psycopg2.connect(DATABASE_URL)
 cur = conn.cursor(cursor_factory=RealDictCursor)
 
+
 class Database:
     '''constructor initialize environment'''
+
     def __init__(self):
         self.conn = psycopg2.connect(DATABASE_URL)
         self.cur = self.conn.cursor(cursor_factory=RealDictCursor)
@@ -18,19 +20,19 @@ class Database:
         invoices = '''CREATE TABLE IF NOT EXISTS invoices(
                         ContactName VARCHAR NOT NULL,
                         InvoiceNumber INT NOT NULL,
-                        InvoiceDate TIMESTAMP NOT NULL,
-                        DueDate TIMESTAMP NOT NULL,
+                        InvoiceDate DATE NOT NULL,
+                        DueDate DATE NOT NULL,
                         Description VARCHAR NOT NULL,
                         Quantity INT NOT NULL,
                         UnitAmount INT NOT NULL
                     );'''
-                
+
         queries = [invoices]
         for q in queries:
             self.cur.execute(q)
             self.conn.commit()
         print("All tables created successfully!")
-        
+
     def drop_tables(self):
         '''Method to drop tables'''
         query = "DROP TABLE invoices;"
@@ -38,8 +40,9 @@ class Database:
         self.conn.commit()
         print("All tables dropped successfully!")
         self.cur.close()
-        
-class Invoice:   
+
+
+class Invoice:
     def __init__(self, ContactName, InvoiceNumber, InvoiceDate, DueDate, Description, Quantity, UnitAmount):
         """invoice constructor"""
         self.ContactName = ContactName
@@ -49,11 +52,15 @@ class Invoice:
         self.Description = Description
         self.Quantity = Quantity
         self.UnitAmount = UnitAmount
-    
+
     @staticmethod
     def get_top_customers():
         """ Fetch top customers using unitamount"""
-        query = "SELECT contactname, (quantity*unitamount) as totalamountdue from invoices ORDER BY unitamount DESC LIMIT 5;"
+        query = "SELECT contactname, date_part('year', duedate) AS Year,\
+                (quantity*unitamount) as totalamountdue from invoices\
+                ORDER BY unitamount DESC LIMIT 5;"
         cur.execute(query)
         invoices = cur.fetchall()
         return invoices
+    
+    
