1482cf90ec5353e5dc62e1c56ac04673bdb82efc
Philipotieno <tmitchellb007@gmail.com>
2020-02-08 12:00:37 +0300

    feat: handle errors

--- app/__init__.py
+++ app/__init__.py
@@ -33,26 +33,26 @@ def create_app(config_name):
                     
                     # Get specific columns and write them on the new csv file
                     writer.writerows(
-                        map(itemgetter(0, 10, 12, 13, 16, 17, 18), reader))
+                        map(itemgetter(0, 10, 12, 13, 16, 17), reader))
                     
-                    # Open new file and copy the items to the invoices table
-                    with open(new_filename, 'r') as f:
-                        reader = csv.reader(f)
-                        cmd = "COPY invoices FROM STDIN WITH (FORMAT CSV, HEADER FALSE, QUOTE E'\b')"
-                        cur.copy_expert(cmd, f)
-                        db.conn.commit()
+                # Open new file and copy the items to the invoices table
+                with open(new_filename, 'r') as r:
+                    reader = csv.reader(r)
+                    cmd = "COPY invoices FROM STDIN WITH (FORMAT CSV, HEADER TRUE)"
+                    cur.copy_expert(cmd, r)
+                    db.conn.commit()
                         
-                        # Covert the csv file into a dictionary
-                        dictreader = csv.reader(open(new_filename))
-                        result = {}
-                        for row in dictreader:
-                            result[row[0]]=row[1:]
-                        
-            return jsonify({
-                'success': True,
-                'invoice': result
-                }), 201
-        except:
-            abort(422)
+                # Covert the csv file into a dictionary
+                dictreader = csv.reader(open(new_filename))
+                result = {}
+                for row in dictreader:
+                    result[row[0]]=row[1:]
+                            
+                return jsonify({
+                    'success': True,
+                    'invoice': result
+                    }), 201
+        except Exception as e:
+            return jsonify({"Error" : str(e)}), 422
 
     return app
